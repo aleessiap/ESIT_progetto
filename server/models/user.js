34f9d02c6e827d3419f946ba2913e49c2b11c71c
FContu <fr.contu@outlook.com>
2021-07-25 11:42:15 +0200

    :modified schemas: user

--- server/models/user.js
+++ server/models/user.js
@@ -7,9 +7,9 @@ const userSchema = new Schema(
 
     name: { type: String, required: true },
     surname: { type: String, required: true },
-    phone_num: { type: String, required: true },
     birthdate: { type: Date, required: true, default: Date.now },
 
+    phone_num: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     admin: { type: Boolean, required: true },
